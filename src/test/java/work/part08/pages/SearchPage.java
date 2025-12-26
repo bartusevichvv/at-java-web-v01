3dd77b8400710399daad4923030da0a70914155c
bartusevichvv <bartusev_sykt@mail.ru>
2025-12-26 10:52:01 +0300

    Update SearchPage.java

--- src/test/java/work/part08/pages/SearchPage.java
+++ src/test/java/work/part08/pages/SearchPage.java
@@ -3,6 +3,7 @@ package work.part08.pages;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
+
 import com.codeborne.selenide.SelenideElement;
 
 public class SearchPage {
@@ -19,7 +20,7 @@ public class SearchPage {
     public void search(String departureDate, String departureMonth, String from, String to) {
         this.tripType.click();
         this.departDay.selectOption(departureDate);
-        this.departMonth.setValue(departureMonth);
+        this.departMonth.selectOption(departureMonth);
         this.cityFrom.selectOption(from);
         this.cityTo.selectOption(to);
         this.continueButton.click();
